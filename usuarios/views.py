f8af60ac93b9a70d1323c2281ac70bddb3a9b3de
Neverton Sousa <nevertonsousa2@gmail.com>
2024-01-17 11:02:43 -0300

    cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Senha e Confirmar senha não coíncidem')
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'usuario ja existe')
+            return redirect('/usuarios/cadastro')
+        try:
+            user = User.objects.create_user(
+                username=username,
+                password=confirmar_senha,
+            )
+            messages.add_message(request, constants.ERROR, 'Usuário cadastrado com sucesso.')
+            return redirect('/usuarios/login')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('/usuarios/cadastro')
